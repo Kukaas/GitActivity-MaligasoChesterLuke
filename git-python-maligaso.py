83c116cd45816ae683f6772bf564128b1e773ce7
MaligasoChesterLuke <chestermaligaso29@gamil.com>
2023-03-08 22:08:48 +0800

    My Educational Background added

--- git-python-maligaso.py
+++ git-python-maligaso.py
@@ -1,4 +1,8 @@
 print("Chester Luke A. Maligaso")
 print("Libtangin, Gasan, Marinduque")
 print("August 29, 2003")
+print("")
+print("Marinduque State College")
+print("Bachelor of Science in Information Technology")
+print("BSIT - 2A")
 
